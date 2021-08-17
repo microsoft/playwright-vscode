65d7e22875b216aa18fae6eedfc26cd02c09225d
Max Schmitt <max@schmitt.mx>
2021-08-17 15:34:07 +0200

    chore: improve readme and documentation (#15)
    Co-authored-by: Andrey Lushnikov <aslushnikov@gmail.com>

--- example/example1.spec.ts
+++ example/example1.spec.ts
@@ -15,47 +15,6 @@
  */
 import { expect, test } from '@playwright/test';
 
-test('should be awesome1', () => {
-  console.log(123);
-  expect(1).toBe(1);
-});
-
-test('1212me', async ({page}) => {
-  expect(await page.evaluate(() => window.navigator.userAgent)).toContain('WebKit');
-});
-
-test.describe('should be awesome²', () => {
-  test('me333', () => {
-    expect(1).toBe(1);
-  });
-  test.describe('layer 2', () => {
-
-    test('you', () => {
-      expect(1).toBe(1);
-    });
-    test('he', () => {
-      expect(1).toBe(1);
-    });
-    test('she123', ({ page }) => {
-      expect(1).toBe(1);
-    });
-    test('it', () => {
-      expect(1).toBe(1);
-    });
-
-    test('but not my cat', () => {
-      expect(1).toBe(1);
-    });
-
-    test('but not my cat234', () => {
-      expect(1).toBe(1);
-    });
-
-    test('but not my cat4', () => {
-      expect(1).toBe(1);
-    });
-    test('but not my cat45', () => {
-      expect(2).toBe(1);
-    });
-  });
+test('should be able to login', async ({page}) => {
+  await page.goto('http://localhost:3000');
 });
