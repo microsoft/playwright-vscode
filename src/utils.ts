e8b9c9f2d2fa2251becf41983d5439b75b4be468
Max Schmitt <max@schmitt.mx>
2022-06-24 11:34:43 +0200

    test

--- src/utils.ts
+++ src/utils.ts
@@ -43,7 +43,7 @@ export async function findInPath(program: string): Promise<string | undefined> {
     if (fs.existsSync(locator)) {
       const stdout = await spawnAsync(locator, [program]);
       const lines = stdout.split(/\r?\n/);
-      console.log(lines);
+      console.log(locator, program, stdout, lines);
 
       if (process.platform === 'win32') {
         // return the first path that has a executable extension
