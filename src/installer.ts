3d848ca5ea6301305424d7fcd6807b922f3641d4
Pavel Feldman <pavel.feldman@gmail.com>
2022-08-16 11:54:36 -0700

    feat(update): suggest installing browsers on update (#218)

--- src/installer.ts
+++ src/installer.ts
@@ -18,45 +18,24 @@ import path from 'path';
 import fs from 'fs';
 import os from 'os';
 import * as vscodeTypes from './vscodeTypes';
+import { TestModel } from './testModel';
 
 export async function installPlaywright(vscode: vscodeTypes.VSCode) {
   const [workspaceFolder] = vscode.workspace.workspaceFolders || [];
   if (!workspaceFolder)
     return;
-  const chromium: vscodeTypes.QuickPickItem = {
-    label: 'Chromium',
-    picked: true,
-    description: '— powers Google Chrome, Microsoft Edge, etc\u2026',
-  };
-  const firefox: vscodeTypes.QuickPickItem = {
-    label: 'Firefox',
-    picked: true,
-    description: '— powers Mozilla Firefox',
-  };
-  const webkit: vscodeTypes.QuickPickItem = {
-    label: 'WebKit',
-    picked: true,
-    description: '— powers  Apple Safari',
-  };
-  const addAction: vscodeTypes.QuickPickItem = {
-    label: 'Add GitHub Actions workflow',
-    picked: true,
-    description: '— adds GitHub Actions recipe'
-  };
-  const installDepsAction: vscodeTypes.QuickPickItem = {
-    label: 'Install Linux dependencies',
-    picked: process.platform === 'linux' && !fs.existsSync(path.join(process.env.XDG_CACHE_HOME || path.join(os.homedir(), '.cache'), 'ms-playwright')),
-  };
   const hasQuickPickSeparator = parseFloat(vscode.version) >= 1.64;
   const options: vscodeTypes.QuickPickItem[] = [];
   if (hasQuickPickSeparator)
     options.push({ label: 'Select browsers to install', kind: vscode.QuickPickItemKind.Separator });
-  options.push(chromium, firefox, webkit);
+  options.push(chromiumItem, firefoxItem, webkitItem);
   if (hasQuickPickSeparator)
     options.push({ label: '', kind: vscode.QuickPickItemKind.Separator });
-  options.push(addAction);
-  if (process.platform === 'linux')
-    options.push(installDepsAction);
+  options.push(addActionItem);
+  if (process.platform === 'linux') {
+    updateInstallDepsPicked();
+    options.push(installDepsItem);
+  }
   const result = await vscode.window.showQuickPick(options, {
     title: 'Install Playwright',
     canPickMany: true,
@@ -73,16 +52,87 @@ export async function installPlaywright(vscode: vscodeTypes.VSCode) {
   terminal.show();
 
   const args: string[] = [];
-  if (result.includes(chromium))
+  if (result.includes(chromiumItem))
     args.push('--browser=chromium');
-  if (result.includes(firefox))
+  if (result.includes(firefoxItem))
     args.push('--browser=firefox');
-  if (result.includes(webkit))
+  if (result.includes(webkitItem))
     args.push('--browser=webkit');
-  if (result.includes(addAction))
+  if (result.includes(addActionItem))
     args.push('--gha');
-  if (result.includes(installDepsAction))
+  if (result.includes(installDepsItem))
     args.push('--install-deps');
 
   terminal.sendText(`npm init --yes playwright@latest -- --quiet ${args.join(' ')}`, true);
+}
+
+export async function installBrowsers(vscode: vscodeTypes.VSCode, model: TestModel) {
+  const hasQuickPickSeparator = parseFloat(vscode.version) >= 1.64;
+  const options: vscodeTypes.QuickPickItem[] = [];
+  if (hasQuickPickSeparator)
+    options.push({ label: 'Select browsers to install', kind: vscode.QuickPickItemKind.Separator });
+  options.push(chromiumItem, firefoxItem, webkitItem);
+  if (hasQuickPickSeparator)
+    options.push({ label: '', kind: vscode.QuickPickItemKind.Separator });
+  if (process.platform === 'linux') {
+    updateInstallDepsPicked();
+    options.push(installDepsItem);
+  }
+  const result = await vscode.window.showQuickPick(options, {
+    title: `Install browsers for Playwright v${model.config.version}:`,
+    canPickMany: true,
+  });
+  if (!result?.length)
+    return;
+
+  const terminal = vscode.window.createTerminal({
+    name: 'Install Playwright',
+    cwd: model.config.workspaceFolder,
+    env: process.env,
+  });
+
+  terminal.show();
+
+  const args: string[] = [];
+  const installCommand = result.includes(installDepsItem) ? 'install-with-deps' : 'install';
+  if (result.includes(chromiumItem))
+    args.push('chromium');
+  if (result.includes(firefoxItem))
+    args.push('firefox');
+  if (result.includes(webkitItem))
+    args.push('webkit');
+
+  if (args.length)
+    terminal.sendText(`npx playwright ${installCommand} ${args.join(' ')}`, true);
+  else if (result.includes(installDepsItem))
+    terminal.sendText(`npx playwright install-deps`, true);
+}
+
+const chromiumItem: vscodeTypes.QuickPickItem = {
+  label: 'Chromium',
+  picked: true,
+  description: '— powers Google Chrome, Microsoft Edge, etc\u2026',
+};
+const firefoxItem: vscodeTypes.QuickPickItem = {
+  label: 'Firefox',
+  picked: true,
+  description: '— powers Mozilla Firefox',
+};
+const webkitItem: vscodeTypes.QuickPickItem = {
+  label: 'WebKit',
+  picked: true,
+  description: '— powers  Apple Safari',
+};
+const addActionItem: vscodeTypes.QuickPickItem = {
+  label: 'Add GitHub Actions workflow',
+  picked: true,
+  description: '— adds GitHub Actions recipe'
+};
+const installDepsItem: vscodeTypes.QuickPickItem = {
+  label: 'Install Linux dependencies',
+  picked: false,
+};
+
+function updateInstallDepsPicked() {
+  installDepsItem.picked = process.platform === 'linux' && !fs.existsSync(path.join(process.env.XDG_CACHE_HOME || path.join(os.homedir(), '.cache'), 'ms-playwright'));
 }
