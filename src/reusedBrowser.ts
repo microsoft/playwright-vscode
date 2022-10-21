2ae1d48d2acf4ff17c72c92fb0d8e4be2d185823
Pavel <pavel.feldman@gmail.com>
2022-10-21 12:56:15 -0700

    chore: no longer need to set reuse mode

--- src/reusedBrowser.ts
+++ src/reusedBrowser.ts
@@ -103,7 +103,7 @@ export class ReusedBrowser implements vscodeTypes.Disposable {
       return;
     }
 
-    const legacyMode = config.version < 1.28;
+    const legacyMode = config.version < 1.29;
 
     const node = await findNode();
     const allArgs = [
@@ -331,7 +331,6 @@ test('test', async ({ page }) => {
     if (!this._checkVersion(config, 'Show & reuse browser'))
       return;
     await this._startBackendIfNeeded(config);
-    await this._backend!.setReuseBrowser({ enabled: true });
     await this._backend!.setAutoClose({ enabled: false });
     this._isRunningTests = true;
   }
@@ -342,7 +341,6 @@ test('test', async ({ page }) => {
       this.stop();
     } else {
       this._backend?.setAutoClose({ enabled: true });
-      this._backend?.setReuseBrowser({ enabled: false });
     }
   }
 
@@ -414,10 +412,6 @@ class Backend extends EventEmitter {
     await this._send('setTrackHierarchy', params);
   }
 
-  async setReuseBrowser(params: { enabled: boolean }) {
-    await this._send('setReuseBrowser', params);
-  }
-
   async setAutoClose(params: { enabled: boolean }) {
   }
 
@@ -482,8 +476,6 @@ class LegacyBackend extends EventEmitter {
   async setAutoClose(params: { enabled: boolean }) {
     await this._send('setAutoClose', params);
   }
-
-  async setReuseBrowser() {}
 
   async highlight(params: { selector: string }) {
     await this._send('highlight', params);
