53ae4f99f6326174a5b5e73541af0401b2645a0a
Max Schmitt <max@schmitt.mx>
2023-08-18 22:05:07 +0200

    fix: show browser when DISPLAY env var is provided

--- src/reusedBrowser.ts
+++ src/reusedBrowser.ts
@@ -334,7 +334,7 @@ export class ReusedBrowser implements vscodeTypes.Disposable {
       return false;
     }
 
-    if (this._vscode.env.uiKind === this._vscode.UIKind.Web) {
+    if (this._vscode.env.uiKind === this._vscode.UIKind.Web && !process.env.DISPLAY) {
       this._vscode.window.showWarningMessage(
           this._vscode.l10n.t('Show browser mode does not work in remote vscode')
       );
