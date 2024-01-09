f0259478b9c418e406a15390a081d0ef13ed03ff
Max Schmitt <max@schmitt.mx>
2024-01-09 17:57:56 +0100

    fix: evaluate 'which' via shell if Node.js is not in PATH (fallback) (#400)

--- src/utils.ts
+++ src/utils.ts
@@ -20,6 +20,7 @@ import fs from 'fs';
 import path from 'path';
 import readline from 'readline';
 import which from 'which';
+import * as vscodeTypes from './vscodeTypes';
 
 export function calculateSha1(buffer: Buffer | string): string {
   const hash = crypto.createHash('sha1');
@@ -138,18 +139,47 @@ export async function resolveSourceMap(file: string, fileToSources: Map<string, 
 
 let pathToNodeJS: string | undefined;
 
-export async function findNode(): Promise<string> {
+export async function findNode(vscode: vscodeTypes.VSCode): Promise<string> {
   if (pathToNodeJS)
     return pathToNodeJS;
 
+  // Stage 1: Try to find Node.js via process.env.PATH
   let node = await which('node').catch(e => undefined);
-  // When extension host boots, it does not have the right env set, so we might need to wait.
+  // Stage 2: When extension host boots, it does not have the right env set, so we might need to wait.
   for (let i = 0; i < 5 && !node; ++i) {
-    await new Promise(f => setTimeout(f, 1000));
+    await new Promise(f => setTimeout(f, 200));
     node = await which('node').catch(e => undefined);
   }
+  // Stage 3: If we still haven't found Node.js, try to find it via a subprocess.
+  // This evaluates shell rc/profile files and makes nvm work.
+  node ??= await findNodeViaShell(vscode);
   if (!node)
     throw new Error('Unable to launch `node`, make sure it is in your PATH');
   pathToNodeJS = node;
   return node;
+}
+
+async function findNodeViaShell(vscode: vscodeTypes.VSCode): Promise<string | undefined> {
+  if (process.platform === 'win32')
+    return undefined;
+  return new Promise<string | undefined>(resolve => {
+    const startToken = '___START_PW_SHELL__';
+    const endToken = '___END_PW_SHELL__';
+    const childProcess = spawn(`${vscode.env.shell} -i -c 'echo ${startToken} && which node && echo ${endToken}'`, {
+      stdio: 'pipe',
+      shell: true
+    });
+    let output = '';
+    childProcess.stdout.on('data', data => output += data.toString());
+    childProcess.on('error', () => resolve(undefined));
+    childProcess.on('exit', exitCode => {
+      if (exitCode !== 0)
+        return resolve(undefined);
+      const start = output.indexOf(startToken);
+      const end = output.indexOf(endToken);
+      if (start === -1 || end === -1)
+        return resolve(undefined);
+      return resolve(output.substring(start + startToken.length, end).trim());
+    });
+  });
 }
