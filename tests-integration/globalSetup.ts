8ec51bb9cadf163325fc1f6a8de9cac5f320df38
Max Schmitt <max@schmitt.mx>
2024-01-15 11:25:14 +0100

    chore: bump @playwright/test to 1.41.0-beta-1705101589000

--- tests-integration/globalSetup.ts
+++ tests-integration/globalSetup.ts
@@ -17,5 +17,4 @@ import { downloadAndUnzipVSCode } from '@vscode/test-electron/out/download';
 
 export default async () => {
   await downloadAndUnzipVSCode('insiders');
-  await downloadAndUnzipVSCode('stable');
 };
