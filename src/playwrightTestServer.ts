308b0537a31977090a0a9983ea0215229eda92c4
Max Schmitt <max@schmitt.mx>
2024-05-18 16:52:47 +0200

    fix: debug on Windows (#484)

--- src/playwrightTestServer.ts
+++ src/playwrightTestServer.ts
@@ -191,8 +191,8 @@ export class PlaywrightTestServer {
 
   async debugTests(items: vscodeTypes.TestItem[], runOptions: PlaywrightTestRunOptions, reporter: reporterTypes.ReporterV2, token: vscodeTypes.CancellationToken): Promise<void> {
     // Important, VSCode will change c:\\ to C:\\ in the program argument.
-    // This forks globals into 2 worlds.
-    const configFile = path.resolve(this._model.config.workspaceFolder, this._model.config.configFile);
+    // This forks globals into 2 worlds, hence we specify it relative than absolute.
+    const configFile = path.relative(this._model.config.workspaceFolder, this._model.config.configFile);
     const args = ['test-server', '-c', configFile];
 
     const addressPromise = new Promise<string>(f => {
