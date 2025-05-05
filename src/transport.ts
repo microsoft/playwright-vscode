5f41ae4045cc2e75fa8beb1bf54eb3167ff632b1
Simon Knott <info@simonknott.de>
2025-05-05 13:50:57 +0200

    chore: remove dead PipeTransport

--- src/transport.ts
+++ src/transport.ts
@@ -38,63 +38,6 @@ export interface ConnectionTransport {
   onclose?: () => void,
 }
 
-export class PipeTransport implements ConnectionTransport {
-  private _pipeWrite: NodeJS.WritableStream;
-  private _pendingMessage = '';
-  private _closed = false;
-
-  onmessage?: (message: ProtocolResponse) => void;
-  onclose?: () => void;
-
-  constructor(pipeWrite: NodeJS.WritableStream, pipeRead: NodeJS.ReadableStream) {
-    this._pipeWrite = pipeWrite;
-    pipeRead.on('data', buffer => this._dispatch(buffer));
-    pipeRead.on('close', () => {
-      this._closed = true;
-      if (this.onclose)
-        this.onclose.call(null);
-    });
-    this.onmessage = undefined;
-    this.onclose = undefined;
-  }
-
-  isClosed(): boolean {
-    return this._closed;
-  }
-
-  send(message: ProtocolRequest) {
-    if (this._closed)
-      throw new Error('Pipe has been closed');
-    this._pipeWrite.write(JSON.stringify(message) + '\0');
-  }
-
-  close() {
-    this._pipeWrite.end();
-  }
-
-  _dispatch(buffer: Buffer) {
-    let end = buffer.indexOf('\0');
-    if (end === -1) {
-      this._pendingMessage += buffer.toString();
-      return;
-    }
-    const message = this._pendingMessage + buffer.toString(undefined, 0, end);
-    if (this.onmessage)
-      this.onmessage.call(null, JSON.parse(message));
-
-    let start = end + 1;
-    end = buffer.indexOf('\0', start);
-    while (end !== -1) {
-      const message = buffer.toString(undefined, start, end);
-      if (this.onmessage)
-        this.onmessage.call(null, JSON.parse(message));
-      start = end + 1;
-      end = buffer.indexOf('\0', start);
-    }
-    this._pendingMessage = buffer.toString(undefined, start);
-  }
-}
-
 export class WebSocketTransport implements ConnectionTransport {
   private _ws: WebSocket;
 
