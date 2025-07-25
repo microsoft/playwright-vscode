6e3d05cd899a0d1fbc50b69aa66e805817a68c1d
Simon Knott <info@simonknott.de>
2025-07-25 17:06:46 +0200

    revert dropdown change

--- src/settingsView.ts
+++ src/settingsView.ts
@@ -258,28 +258,32 @@ function htmlForWebview(vscode: vscodeTypes.VSCode, extensionUri: vscodeTypes.Ur
             <div>${vscode.l10n.t('Run global setup on each run')}</div>
           </label>
         </div>
-        <div class="action">
-          <label class="dropdown">
-            ${vscode.l10n.t('Update snapshots:')}
-            <select setting="updateSnapshots" class="combobox">
-              <option value="no-override">no override</option>
-              <option value="all">all</option>
-              <option value="changed">changed</option>
-              <option value="missing">missing</option>
-              <option value="none">none</option>
-            </select>
-          </label>
+        <div class="hbox">
+          <label id="updateSnapshotLabel">${vscode.l10n.t('Update snapshots')}</label>
         </div>
-        <div class="action">
-          <label class="dropdown">
-            ${vscode.l10n.t('Update method:')}
-            <select setting="updateSourceMethod" class="combobox">
-              <option value="no-override">no override</option>
-              <option value="overwrite">overwrite</option>
-              <option value="patch">patch</option>
-              <option value="3way">3-way</option>
-            </select>
-          </label>
+        <div class="combobox">
+          <div class="action-big-indent"></div>
+          <select setting="updateSnapshots" aria-labelledby="updateSnapshotLabel">
+            <option value="no-override">no override</option>
+            <option value="all">all</option>
+            <option value="changed">changed</option>
+            <option value="missing">missing</option>
+            <option value="none">none</option>
+          </select>
+          <div class="action-indent"></div>
+        </div>
+        <div class="hbox">
+          <label id="updateSourceMethod">${vscode.l10n.t('Update method')}</label>
+        </div>
+        <div class="combobox">
+          <div class="action-big-indent"></div>
+          <select setting="updateSourceMethod" aria-labelledby="updateSourceMethod">
+            <option value="no-override">no override</option>
+            <option value="overwrite">overwrite</option>
+            <option value="patch">patch</option>
+            <option value="3way">3-way</option>
+          </select>
+          <div class="action-indent"></div>
         </div>
       </div>
     </body>
