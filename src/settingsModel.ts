f6b6131f1243bd6d9947b44e7aeabe077f1d74a3
Yury Semikhatsky <yurys@chromium.org>
2025-09-04 12:15:51 -0700

    fix: disable settings while tests are running (#683)
    Co-authored-by: Simon Knott <info@simonknott.de>

--- src/settingsModel.ts
+++ src/settingsModel.ts
@@ -106,6 +106,7 @@ export class SettingsModel extends DisposableBase {
 }
 
 export interface Setting<T> extends vscodeTypes.Disposable {
+  readonly settingName: string;
   readonly onChange: vscodeTypes.Event<T>;
   get(): T | undefined;
   set(value: T): Promise<void>;
